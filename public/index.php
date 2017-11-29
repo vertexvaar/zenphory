93aad6a1069893189d091c73cf3f17af2d45d4ca
Oliver Eglseder <php@vxvr.de>
2017-11-30 00:50:47 +0100

    [FEATURE] Support multiple installation and execution paths

--- public/index.php
+++ public/index.php
@@ -1,6 +1,19 @@
 <?php
 declare(strict_types=1);
-require_once(__DIR__ . '/../vendor/autoload.php');
+$candidates = [
+    __DIR__ . '/../vendor/autoload.php',
+    __DIR__ . '/../../../autoload.php',
+    getcwd() . '/../vendor/autoload.php',
+    dirname(dirname($_SERVER['SCRIPT_FILENAME'])) . '/vendor/autoload.php',
+    dirname(dirname(dirname(dirname($_SERVER['SCRIPT_FILENAME'])))) . '/autoload.php'
+];
+foreach ($candidates as $candidate) {
+    if (file_exists($candidate)) {
+        require_once($candidate);
+        break;
+    }
+}
+
 require_once(__DIR__ . '/../settings.php');
 $source = __DIR__ . '/../data/source/variables.php';
 if (!file_exists(__DIR__ . '/../data/target/variables.php')) {
@@ -8,7 +21,7 @@ if (!file_exists(__DIR__ . '/../data/target/variables.php')) {
 }
 $target = __DIR__ . '/../data/target/variables.php';
 $code = file_get_contents($source);
-$codeBender = new \VerteXVaaR\Zenphory\Service\CodeBender($source);
+$codeBender = new \VerteXVaaR\Zenphory\Service\CodeBender();
 $code = $codeBender->process($code);
 file_put_contents($target, $code);
 ?>
